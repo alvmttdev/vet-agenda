92040d25546cd250fefa6bd101044114987c37db
alvmttdev <mattosdev@outlook.com>
2026-05-08 17:43:11 -0300

    Arrumado estrutura de arquivos e conecta com sucesso ao MySQL

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+// Faz a ponte com o arquivo de banco de dados
+require('./src/config/database');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Bem-vindo a API da VetAgenda! O servidor esta funcionando.');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Servidor a rodar com sucesso na porta ${PORT}`);
+});
